bf1dfcc01938be79bbbca4840fbb1aab94931e53
scottys88 <scott_schubert@hotmail.com.au>
2018-11-01 20:54:24 +1030

    Up to episode 7 (i think)

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: `660354337537-kf8bdc5rp5gf5vpi9b7fcn85dmbvs0tk.apps.googleusercontent.com`,
+        clientSecret: `RSLl9NpE80VUpE4bZ5vk6K-s`
+    }
+}
